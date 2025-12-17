30a7647282d5478da724fea5faa90351f03cc82b
Nima3516 <unlakshan@gmail.com>
2025-12-17 11:40:06 +0530

    Test auto deploynment

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "CI Pipeline Trigger Test nEW "
+	return "Phase - 4 - Auto Deploynment Successful"
 
 if __name__=="__main__":
 	app.run(host-"0.0.0.0", port=5000)
