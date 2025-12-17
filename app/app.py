3250d64ffc019ed257dce66a7fd9dce9a1486f64
Nima3516 <unlakshan@gmail.com>
2025-12-17 11:49:53 +0530

    Test auto deploynment 2

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Phase - 4 - Auto Deploynment Successful"
+	return "Phase - 4 - Auto Deploynment Successful New One"
 
 if __name__=="__main__":
 	app.run(host-"0.0.0.0", port=5000)
